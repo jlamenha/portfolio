c4f3eff63e7c724e2f512af52768b973eed41837
jlamenha <lamenhajoaolucas@gmail.com>
2024-11-13 20:47:18 +0100

    add eslint-disable for shadcn errors

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -17,7 +17,7 @@ const ToggleGroupContext = React.createContext<
 const ToggleGroup = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
-    VariantProps<typeof toggleVariants>
+  VariantProps<typeof toggleVariants>
 >(({ className, variant, size, children, ...props }, ref) => (
   <ToggleGroupPrimitive.Root
     ref={ref}
@@ -35,7 +35,7 @@ ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName
 const ToggleGroupItem = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
-    VariantProps<typeof toggleVariants>
+  VariantProps<typeof toggleVariants>
 >(({ className, children, variant, size, ...props }, ref) => {
   const context = React.useContext(ToggleGroupContext)
 
@@ -44,7 +44,9 @@ const ToggleGroupItem = React.forwardRef<
       ref={ref}
       className={cn(
         toggleVariants({
+          // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
           variant: context.variant || variant,
+          // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
           size: context.size || size,
         }),
         className
