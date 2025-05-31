ddfc2a91c769b13c1fc92a1fcd8032e1ded9daff
jlamenha <lamenhajoaolucas@gmail.com>
2025-05-30 21:42:09 -0400

    Essentially done (just populate the projects)

--- src/app/chatvpc/page.tsx
+++ src/app/chatvpc/page.tsx
@@ -1,8 +1,24 @@
-
+import ProjectPage from "@/components/project_page/ProjectPage"
 
 export default async function ChatVPCPage() {
   return(
 
-  <p className="text-center"> Non c&apos;è ancora niente qui </p>
+  <ProjectPage
+  title="ChatVPC"
+  description="SQL-to-NoSQL translation using LLMs"
+  tech={["Next.js", "TypeScript", "LLMs"]}
+  githubLink="https://github.com/example/snapt"
+  pdfUrl="/pdfs/snapt.pdf"
+  sections={[
+    {
+      heading: "Motivation",
+      content: `Helping developers migrate from SQL to MongoDB with minimal friction.`,
+    },
+    {
+      heading: "Results",
+      content: `We achieved over 80% accuracy on benchmark query sets.`,
+    },
+  ]}
+/>
   )
 }
