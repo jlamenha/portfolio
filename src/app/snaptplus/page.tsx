f3367882330fd9ffcf5464be238769d478ad27c1
jlamenha <lamenhajoaolucas@gmail.com>
2025-05-31 15:22:41 -0400

    lint &apos; error

--- src/app/snaptplus/page.tsx
+++ src/app/snaptplus/page.tsx
@@ -3,7 +3,7 @@
 export default async function ChatVPCPage() {
   return(
 
-  <p className="text-center"> If you're seeing this, I am not adding SNAPTPlus until my Thesis Advisors explicitly allow me to :p</p>
+  <p className="text-center"> If you&apos;re seeing this, I am not adding SNAPTPlus until my Thesis Advisors explicitly allow me to :p</p>
   )
 }
 
